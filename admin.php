dc3419cff2f4feab4249743b0c7be401f28fb219
meou2731999 <meou2731999@gmail.com>
2020-12-29 21:30:40 +0700

    Edit Product and Add Product

--- admin.php
+++ admin.php
@@ -65,7 +65,7 @@
     <div class="main_content">
         <div id="products">
             <div class="sub_title">
-                <h2>Sản phẩm</h2>
+                <h2>Người dùng</h2>
             </div>
             <div class="projects_content">
                 <table id="admin">
@@ -87,127 +87,25 @@
                             <button class="button_edit" onclick="openModalEditProfile()">Edit</button>
                         </td>
                     </tr>
-                    <tr>
-                        <td>Berglunds snabbköp</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Ernst Handel</td>
-                        <td>Roland Mendel</td>
-                        <td>Austria</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Island Trading</td>
-                        <td>Helen Bennett</td>
-                        <td>UK</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Königlich Essen</td>
-                        <td>Philip Cramer</td>
-                        <td>Germany</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Berglunds snabbköp</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Ernst Handel</td>
-                        <td>Roland Mendel</td>
-                        <td>Austria</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Island Trading</td>
-                        <td>Helen Bennett</td>
-                        <td>UK</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Königlich Essen</td>
-                        <td>Philip Cramer</td>
-                        <td>Germany</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Berglunds snabbköp</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Ernst Handel</td>
-                        <td>Roland Mendel</td>
-                        <td>Austria</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Island Trading</td>
-                        <td>Helen Bennett</td>
-                        <td>UK</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Königlich Essen</td>
-                        <td>Philip Cramer</td>
-                        <td>Germany</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
                 </table>
 
             </div>
         </div>
         <div id="users">
             <div class="sub_title">
-                <h2>Người dùng</h2>
+                <h2>Sản phẩm</h2>
             </div>
             <div class="projects_content">
+                <button class="signup-btn" onclick="openModalAddProducts()">Thêm sản phẩm</button>
                 <table id="admin">
                     <tr>
-                        <th>Tên đăng nhập</th>
-                        <th>Ngày sinh</th>
-                        <th>Giới tính</th>
-                        <th>Email</th>
-                        <th>Số điện thoại</th>
+                        <th>Tên sản phẩm</th>
+                        <th>Danh mục</th>
+                        <th>Địa chỉ</th>
+                        <th>Mô tả</th>
+                        <th>Giá</th>
+                        <th>Người đăng</th>
+                        <th>Thao tác</th>
                     </tr>
                     <tr>
                         <td>Alfreds Futterkiste</td>
@@ -215,111 +113,10 @@
                         <td>Germany</td>
                         <td>Christina Berglund</td>
                         <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Berglunds snabbköp</td>
                         <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Ernst Handel</td>
-                        <td>Roland Mendel</td>
-                        <td>Austria</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Island Trading</td>
-                        <td>Helen Bennett</td>
-                        <td>UK</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Königlich Essen</td>
-                        <td>Philip Cramer</td>
-                        <td>Germany</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Berglunds snabbköp</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Ernst Handel</td>
-                        <td>Roland Mendel</td>
-                        <td>Austria</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Island Trading</td>
-                        <td>Helen Bennett</td>
-                        <td>UK</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Königlich Essen</td>
-                        <td>Philip Cramer</td>
-                        <td>Germany</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Berglunds snabbköp</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Ernst Handel</td>
-                        <td>Roland Mendel</td>
-                        <td>Austria</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Island Trading</td>
-                        <td>Helen Bennett</td>
-                        <td>UK</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
-                    </tr>
-                    <tr>
-                        <td>Königlich Essen</td>
-                        <td>Philip Cramer</td>
-                        <td>Germany</td>
-                        <td>Christina Berglund</td>
-                        <td>Sweden</td>
+                        <td style="text-align: center;">
+                            <button class="button_edit" onclick="openModalEditProducts()">Edit</button>
+                        </td>
                     </tr>
                 </table>
             </div>
@@ -350,6 +147,54 @@
                     <div style="display: flex;justify-content: center;">
                         <input type="submit" value="Lưu">
                         <button class="signup-btn">Xoá</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div id="editProductsModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="button" style="margin-top: 18px;">
+                    <span class="close">&times;</span>
+                </div>
+                <h2 style="margin-top: 20px !important;">Chỉnh sửa</h2>
+            </div>
+            <div class="modal-body">
+                <form action="editproduct.php" method="POST" style="width: 100%;" enctype="multipart/form-data">
+                    <input type="hidden" id="id" name="id" value="<?php echo $userid_session; ?>">
+                    <input type="text" id="title" name="title" placeholder="<?php echo $login_session; ?>" required>
+                    <input type="text" id="category" name="category" placeholder="<?php echo $category; ?>">
+                    <input type="text" id="address" name="address" value="<?php echo $address; ?>">
+                    <textarea name="content" rows="10"><?php echo $category; ?></textarea>
+                    <input type="number" id="cost" name="cost" placeholder="<?php echo $cost; ?>">
+                    <input type="file" id="img" name="img" placeholder="Picture">
+                    <div style="display: flex;justify-content: center;">
+                        <input type="submit" value="Lưu">
+                        <button class="signup-btn">Xoá</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div id="addProductsModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="button" style="margin-top: 18px;">
+                    <span class="close">&times;</span>
+                </div>
+                <h2 style="margin-top: 20px !important;">Tạo mới</h2>
+            </div>
+            <div class="modal-body">
+                <form action="editproduct.php" method="POST" style="width: 100%;" enctype="multipart/form-data">
+                    <input type="text" id="title" name="title" placeholder="Tên sản phẩm" required>
+                    <input type="text" id="category" name="category" placeholder="Danh mục sản phẩm">
+                    <input type="text" id="address" name="address" placeholder="Địa chỉ">
+                    <textarea name="content" rows="10" placeholder="Nội dung mô tả sản phẩm"></textarea>
+                    <input type="number" id="cost" name="cost" placeholder="Giá trị sản phẩm">
+                    <input type="file" id="img" name="img" placeholder="Hình ảnh sản phẩm">
+                    <div style="display: flex;justify-content: center;">
+                        <input type="submit" value="Lưu">
                     </div>
                 </form>
             </div>
@@ -394,9 +239,45 @@
         spanEditProfile.onclick = function() {
             modalEditProfile.style.display = "none";
         }
+
+        var modalEditProducts = document.getElementById("editProductsModal");
+
+        // Get the <span> element that closes the modalEditProducts
+        var spanEditProducts = document.getElementsByClassName("close")[1];
+
+        // When the user clicks the button, open the modalEditProducts 
+        openModalEditProducts = function() {
+            modalEditProducts.style.display = "block";
+            myFunction();
+        }
+        // When the user clicks on <span> (x), close the modalEditProducts
+        spanEditProducts.onclick = function() {
+            modalEditProducts.style.display = "none";
+        }
+
+        var modalAddProducts = document.getElementById("addProductsModal");
+
+        // Get the <span> element that closes the modalAddProducts
+        var spanAddProducts = document.getElementsByClassName("close")[2];
+
+        // When the user clicks the button, open the modalAddProducts 
+        openModalAddProducts = function() {
+            modalAddProducts.style.display = "block";
+            myFunction();
+        }
+        // When the user clicks on <span> (x), close the modalAddProducts
+        spanAddProducts.onclick = function() {
+            modalAddProducts.style.display = "none";
+        }
         window.onclick = function(event) {
             if (event.target == modalEditProfile) {
                 modalEditProfile.style.display = "none";
+            }
+            if (event.target == modalEditProducts) {
+                modalEditProducts.style.display = "none";
+            }
+            if (event.target == modalAddProducts) {
+                modalAddProducts.style.display = "none";
             }
         }
     </script>
